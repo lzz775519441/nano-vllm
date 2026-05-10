5654d080ce7e0f980800a05411d3753a5a4648fd
lzz775519441 <lzz775519441@gmail.com>
2026-05-10 17:52:06 +0800

    update loaded

--- nanovllm/utils/loader.py
+++ nanovllm/utils/loader.py
@@ -5,41 +5,39 @@ from torch import nn
 from safetensors import safe_open
 
 
-def default_weight_loader(param: nn.Parameter | torch.Tensor, loaded_weight: torch.Tensor):
+def default_weight_loader(param: nn.Parameter | torch.Tensor,
+                          loaded_weight: torch.Tensor):
     param.data.copy_(loaded_weight)
-
-
-def get_parameter_or_buffer(model: nn.Module, name: str) -> nn.Parameter | torch.Tensor:
-    parameters = dict(model.named_parameters())
-    if name in parameters:
-        return parameters[name]
-    return dict(model.named_buffers())[name]
 
 
 def post_init_gptq_modules(model: nn.Module):
     from gptqmodel.nn_modules.qlinear.marlin import MarlinLinear
 
     for module in model.modules():
-        if not isinstance(module, MarlinLinear):
-            continue
-        module.post_init()
+        if isinstance(module, MarlinLinear):
+            module.post_init()
 
 
 def load_model(model: nn.Module, path: str):
     packed_modules_mapping = getattr(model, "packed_modules_mapping", {})
-    for file in glob(os.path.join(path, "*.safetensors")):
+
+    params = dict(model.named_parameters())
+    params.update(dict(model.named_buffers()))
+
+    for file in sorted(glob(os.path.join(path, "*.safetensors"))):
         with safe_open(file, "pt", "cpu") as f:
             for weight_name in f.keys():
-                for k in packed_modules_mapping:
+                for k, (v, shard_id) in packed_modules_mapping.items():
                     if k in weight_name:
-                        v, shard_id = packed_modules_mapping[k]
                         param_name = weight_name.replace(k, v)
-                        param = get_parameter_or_buffer(model, param_name)
-                        weight_loader = getattr(param, "weight_loader")
-                        weight_loader(param, f.get_tensor(weight_name), shard_id)
+                        param = params[param_name]
+                        param.weight_loader(param, f.get_tensor(weight_name),
+                                            shard_id)
                         break
                 else:
-                    param = get_parameter_or_buffer(model, weight_name)
-                    weight_loader = getattr(param, "weight_loader", default_weight_loader)
+                    param = params[weight_name]
+                    weight_loader = getattr(param, "weight_loader",
+                                            default_weight_loader)
                     weight_loader(param, f.get_tensor(weight_name))
+
     post_init_gptq_modules(model)
