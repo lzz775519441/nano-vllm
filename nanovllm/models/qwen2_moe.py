face97cd36f79a532d82cbe1003f2497900bb56b
lzz775519441 <lzz775519441@gmail.com>
2026-05-10 17:41:16 +0800

    rope_parameters fix

--- nanovllm/models/qwen2_moe.py
+++ nanovllm/models/qwen2_moe.py
@@ -174,7 +174,7 @@ class Qwen2MoeDecoderLayer(nn.Module):
             num_heads=config.num_attention_heads,
             num_kv_heads=config.num_key_value_heads,
             max_position=config.max_position_embeddings,
-            rope_theta=config.rope_theta,
+            rope_theta=config.rope_parameters["rope_theta"],
             config=config,
         )
         self.mlp = Qwen2MoeSparseMoeBlock(config)
